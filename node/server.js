5f035d1d41a94fdcccddadab87eedcf761b02e5d
Seuun <seunkikiowo@gmail.com>
2019-09-03 21:56:27 +0100

    connected with expressjs and did some get and post request

--- node/server.js
+++ node/server.js
@@ -1,16 +1,53 @@
-const http = require('http')
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-    // response.setHeader('Content-Type', 'text/html')
-    console.log('method', request.method);
-    console.log('url', request.url);
-    const user = {
-        name: 'seun',
-        hobby: 'sleep'
+const app = express();
+
+app.listen(3000);
+
+app.get('/about', (req, res) => {
+    res.send('about us')
+});
+
+app.get('/profile', (req, res) => {
+    res.send('getting profile')
+});
+
+
+app.post('/profile', (req, res) => {
+
+    const user =  {
+        name: 'sally',
+        hobby: 'chilling'
     }
 
-    response.setHeader('Content-Type', 'application/json')
-    response.end(JSON.stringify(user))
+
+    res.send(user);
 })
 
-server.listen(3000)
+
+
+
+
+
+
+
+
+//CREATING SERVER USING NODE HTTP
+
+// const http = require('http')
+
+// const server = http.createServer((request, response) => {
+//     // response.setHeader('Content-Type', 'text/html')
+//     console.log('method', request.method);
+//     console.log('url', request.url);
+
+//     const user = {
+//         name: 'seun',
+//         hobby: 'sleep'
+//     }
+
+//     response.setHeader('Content-Type', 'application/json')
+//     response.end(JSON.stringify(user))
+// })
+
+// server.listen(3000)
