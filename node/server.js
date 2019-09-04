b16e457e3b72488c388b7c9a6c777eb912b83148
Seuun <seunkikiowo@gmail.com>
2019-09-04 23:00:55 +0100

    serving static html with expressJS

--- node/server.js
+++ node/server.js
@@ -3,28 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'))
+
+
+
 
 app.listen(3000);
-
-app.get('/about', (req, res) => {
-    res.send('about us')
-});
-
-app.get('/profile', (req, res) => {
-    res.send('getting profile')
-});
-
-app.post('/profile', (req, res) => {
-    console.log(req.body);
-    const user =  {
-        name: 'sally',
-        hobby: 'chilling'
-    }
-
-    res.send(user);
-})
 
 
 
@@ -53,3 +37,64 @@ app.post('/profile', (req, res) => {
 // })
 
 // server.listen(3000)
+
+
+
+// CREATING A SERVER USING EXPRESS JS
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+// app.listen(3000);
+
+// app.get('/about', (req, res) => {
+//     res.send('about us')
+// });
+
+// app.get('/profile', (req, res) => {
+//     res.send('getting profile')
+// });
+
+// app.post('/profile', (req, res) => {
+//     console.log(req.body);
+//     const user =  {
+//         name: 'sally',
+//         hobby: 'chilling'
+//     }
+
+//     res.send(user);
+// })
+
+
+
+
+// REQ METHODS --- PARAMS, QUERY, BODYPARSER, HEADER
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+
+// const app = express();
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+
+// app.get('/:id', (req, res) => {
+//     // URL address to trigger res.query
+//     // http://127.0.0.1:3000/?name=seun&surname=kikiowo
+//     // console.log(req.query)
+ 
+//     // req.bodyParser
+//     // console.log(req.header)
+//     console.log(req.params)
+
+//     res.status(404).send('not found')
+// })
+
+// app.listen(3000);
+
