cd9c5f759fd068d70921123e2ce9068c0add18af
Seuun <seunkikiowo@gmail.com>
2019-09-11 23:57:10 +0100

    added Tachyons

--- faceapp/src/App.js
+++ faceapp/src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/navigation';
+
+
 import './App.css';
 
 class App extends Component {
