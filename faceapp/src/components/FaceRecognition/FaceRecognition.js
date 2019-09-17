aa6708641e2f7d9544ba7406635899561e74e495
Seuun <seunkikiowo@gmail.com>
2019-09-17 23:48:23 +0100

    adding bounding box

--- faceapp/src/components/FaceRecognition/FaceRecognition.js
+++ faceapp/src/components/FaceRecognition/FaceRecognition.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import './FaceRecognition.css';
 
-const FaceRecognition = ({ imageUrl }) => {
+const FaceRecognition = ({ imageUrl, box }) => {
    return(
        <div className='center ma'>
            <div className='absolute mt2'>
             <img alt="" src={ imageUrl } width='500px' height='auto' id='inputImage'/>
+            <div className='bounding-box' style={{ top: box.topCol, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
            </div>
        </div>
    ) 
